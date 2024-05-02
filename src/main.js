2463170fa66f040451bd0712eb77bc4700540934
Samantha-K-Sinnerine <sksinnerine@gmail.com>
2024-05-02 19:53:28 -0400

    added player object

--- src/main.js
+++ src/main.js
@@ -14,24 +14,60 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
     },
 });
 
-k.loadSprite("map", "./map.png");
+k.loadSprite("map1", "./map1.png");
 
 k.setBackground(k.Color.fromHex("#311047"));
 
 k.scene("main", async () => {
-    const mapData = await (await fetch("./map.json")).json();
+    const mapData = await (await fetch("./map1.json")).json();
     const layers = mapData.layers;
 
     const map = k.make([
-        k.sprite("map"),
+        k.sprite("map1"),
         //position game object will display on screen
         k.pos(0),
-        k.scale(scaleFactor)]);
+        k.scale(scaleFactor)
+    ]); //scale the map to fit the screen
     
     const player = k.make([
-        k.sprite("spritesheet, {anim: "idle-down"}), 
-        k.area)
-    });
+        k.sprite("spritesheet", {anim: "idle-down"}), 
+        k.area({
+            //hitbox for player
+            shape: new k.Rect(k.vec2(0, 3), 10, 10)
+        }),
+        k.body(),
+        k.anchor("center"),
+        k.pos(),
+        k.scale(scaleFactor),
+        {
+            speed: 250,
+            direction: "down",
+            isInDialog: false,
+        },
+        "player",
+    ]);
+
+    for (const layer of layers) {
+        if (layer.name === "boundaries") {
+            for (const boundary of layer.objects) {
+                map.add([
+                    k.area({
+                        shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),                       
+                    }),
+                    // isStatic: true means the player can't overlap with the boundary
+                    k.body({ isStatic: true }),
+                    k.pos(boundary.x, boundary.y),
+                    boundary.name, //tag of game object
+                ]);
+                
+                if (boundary.name) {
+                    player
+                }
+            }
+        }
+    }
+});
+
 
 
 // default scene to start with
