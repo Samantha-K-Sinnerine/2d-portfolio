d17157a881731c0e8b8a43df7fecfa10540c656d
Samantha-K-Sinnerine <sksinnerine@gmail.com>
2024-05-03 00:28:13 -0400

    added walking animations for the player

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
-import { displayDialogue } from "./utils";
+import { displayDialogue, setCamScale } from "./utils";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39, //length of image divided by size of frame = # of frams
@@ -84,15 +84,62 @@ k.scene("main", async () => {
         }
     }
 
+    setCamScale(k);
+
+    k.onResize(() => {
+        setCamScale(k);
+    });
+
     k.onUpdate(() => {
         k.camPos(player.pos.x, player.pos.y + 100);
     });
 
     k.onMouseDown((mouseBtn) => {
         if (mouseBtn != "left" || player.isInDialog) return;
-        
+
         const worldMousePos = k.toWorld(k.mousePos());
         player.moveTo(worldMousePos, player.speed);
+
+        const mouseAngle = player.pos.angle(worldMousePos);
+
+        const lowerBound = 50;
+        const upperBound = 125;
+
+        if (
+            mouseAngle > lowerBound &&
+            mouseAngle < upperBound &&
+            player.curAnim() !== "walk-up"
+        ) {
+            player.play("walk-up");
+            player.direction = "up";
+            return;
+        }
+      
+        if (
+            mouseAngle < -lowerBound &&
+            mouseAngle > -upperBound &&
+            player.curAnim() !== "walk-down"
+        ) {
+            player.play("walk-down");
+            player.direction = "down";
+            return;
+        }
+      
+        if (Math.abs(mouseAngle) > upperBound) {
+            player.flipX = false;
+            if (player.curAnim() !== "walk-side") player.play("walk-side");
+            player.direction = "right";
+            return;
+        }
+      
+        if (Math.abs(mouseAngle) < lowerBound) {
+            player.flipX = true;
+            if (player.curAnim() !== "walk-side") player.play("walk-side");
+            player.direction = "left";
+            return;
+        }
+
+
     });
 });
 
