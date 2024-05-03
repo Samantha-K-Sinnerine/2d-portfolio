293f582a1828f3b9e67603a2ed29572cc9c88207
Samantha-K-Sinnerine <sksinnerine@gmail.com>
2024-05-03 10:50:13 -0400

    added new objects in the boundaries layer

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,15 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc : "Hello, I am a PC.",
-    sofa_table: "This is a sofa table.",
-    table: "This is a table.",
-    library: "This is a library.",
-    cs_degree: "This is a CS degree.",
-    resume: "This is a resume.",
-    projects: "This is a projects.",
-    bed: "This is a bed."
+    pc: "This is my workstation where I specialize in  Python, Java, React.js and Node.js. A few frameworks and backend tools I've used in the past are Flask, Docker, PostgreSQL and MangoDB.",
+    sofa_table: "This is a side table, often a spot for me to relax with psychology books and non-fiction reads, offering insights beyond the realm of code.",
+    table: "This sturdy table has witnessed countless study sessions and collaborations, fostering my skills in data structures, algorithms, and system design.",
+    library: "This is a collection of knowledge, including my CS degree earned from CUNY Queens College in May 2024. It symbolizes my dedication and expertise in computer science.",
+    cs_degree: "My CS degree represents years of dedication and learning, showcasing my readiness to contribute meaningfully to the tech industry.",
+    resume: "This is my professional resume, reflecting my skills, experiences, and achievements. For more details, visit my LinkedIn profile: <a href='https://www.linkedin.com/in/sksinnerine/'>LinkedIn</a>.",
+    projects: "These are my standout projects, demonstrating my skills and creativity. Explore my GitHub profile for a closer look: <a href='https://github.com/Samantha-K-Sinnerine'>GitHub</a>.",
+    bed: "This is my bed—a place of rest and rejuvenation after long days of coding and exploration."
 }
+
+
+
