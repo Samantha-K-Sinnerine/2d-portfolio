36805588be974901e6ccbac0f407a5f348c05766
Samantha-K-Sinnerine <sksinnerine@gmail.com>
2024-05-03 11:05:44 -0400

    edited map1 and constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,15 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: "This is my workstation where I specialize in  Python, Java, React.js and Node.js. A few frameworks and backend tools I've used in the past are Flask, Docker, PostgreSQL and MangoDB.",
-    sofa_table: "This is a side table, often a spot for me to relax with psychology books and non-fiction reads, offering insights beyond the realm of code.",
+    pc: "This is my pc where I specialize in  Python, Java, React.js and Node.js. A few frameworks and backend tools I've used in the past are Flask, Docker, PostgreSQL and MangoDB. Currently I'm learning full-stack development using Java Spring Boot, React, and MongoDB. <br> <br> Explore my <a href='https://github.com/Samantha-K-Sinnerine'>GitHub</a> profile for a closer look.",
+    sofa_table: "This is my sofa, a place where I relax and unwind by watching my favorite k-dramas. ",
     table: "This sturdy table has witnessed countless study sessions and collaborations, fostering my skills in data structures, algorithms, and system design.",
-    library: "This is a collection of knowledge, including my CS degree earned from CUNY Queens College in May 2024. It symbolizes my dedication and expertise in computer science.",
+    library: "This is my library often a spot for me to relax with psychology books and non-fiction reads",
     cs_degree: "My CS degree represents years of dedication and learning, showcasing my readiness to contribute meaningfully to the tech industry.",
     resume: "This is my professional resume, reflecting my skills, experiences, and achievements. For more details, visit my LinkedIn profile: <a href='https://www.linkedin.com/in/sksinnerine/'>LinkedIn</a>.",
-    projects: "These are my standout projects, demonstrating my skills and creativity. Explore my GitHub profile for a closer look: <a href='https://github.com/Samantha-K-Sinnerine'>GitHub</a>.",
-    bed: "This is my bed—a place of rest and rejuvenation after long days of coding and exploration."
+    projects: "These are my standout projects, demonstrating my skills and creativity.",
+    bed: "This is my bed—a place of rest and rejuvenation after long days of coding and exploration.",
+    sewing_machine: "This is my sewing machine. I love upcycling clothes and turning old garments into something new. It's a creative outlet that complements my technical skills.",
 }
 
 
